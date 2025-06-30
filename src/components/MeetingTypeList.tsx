cf04cdb2f2d79e0cd7cacfbefb74b2dc6dbe9f26
Omkar Padalkar <82218332+omkarpadalkar21@users.noreply.github.com>
2025-06-30 21:12:11 +0530

    Add Personal Meeting rooms and Join meeting

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -9,6 +9,7 @@ import { useUser } from "@clerk/nextjs";
 import { toast } from "sonner";
 import { Textarea } from "./ui/textarea";
 import ReactDatePicker from "react-datepicker";
+import { Input } from "./ui/input";
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
@@ -149,9 +150,26 @@ const MeetingTypeList = () => {
         onClose={() => setMeetingState(undefined)}
         title={"Start an Instant Meeting"}
         className={"text-center"}
-        buttonText={"Start meeting"}
+        buttonText={"Start Meeting"}
         handleClick={createMeeting}
       />
+
+      <MeetingModal
+        isOpen={meetingState === "isJoiningMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title={"Type the link here"}
+        className={"text-center"}
+        buttonText={"Join Meeting"}
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder="Meeting link"
+          className="border-none bg-[var(--color-dark-3)] focus-visible:ring-0 focus-visible:ring-offset-0"
+          onChange={(e) => {
+            setValues({ ...values, link: e.target.value });
+          }}
+        />
+      </MeetingModal>
     </section>
   );
 };
